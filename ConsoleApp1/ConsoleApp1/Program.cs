ddea85fc413cd928f48913ee58f61e521d74a635
CenturionDi <40587389+CenturionDi@users.noreply.github.com>
2018-10-03 23:52:21 +0600

    Пересдача 2   1 домашняя работа
    теперь вроде понял

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.Now;
+            Console.WriteLine($"Date: {date:d} Time:{date:t}!");
+            Console.WriteLine("\nHello world! I was born! ");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+
+        }
+    }
+}
