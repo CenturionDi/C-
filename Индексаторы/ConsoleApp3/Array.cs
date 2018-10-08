0e5b4dada93428b1a3398eb6f0397858d82acbe9
CenturionDi <40587389+CenturionDi@users.noreply.github.com>
2018-10-08 21:58:00 +0600

    Add files via upload

---
+++ Индексаторы/ConsoleApp3/Array.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Array
+    {
+        private int[] array;
+        public int Size { get; set; }
+
+        public Array()
+        {
+
+            Size = 1;
+            array = new int[Size];
+        }
+
+        public Array(int size)
+        {
+
+            Size = size;
+            array = new int[Size];
+        }
+        public int this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = (int)Math.Pow((double)value, 2); }
+        }
+
+     
+
+
+
+    }
+}
---
+++ Индексаторы/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array(2);
+            Console.WriteLine("Enter value for first");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for second");
+            int b = Convert.ToInt32(Console.ReadLine());
+            array[0] = a;
+            array[1] = b;
+           
+            for (int i = 0; i < array.Size; i++)
+            {
+                Console.WriteLine("Element: "+i);
+                Console.Write(array[i]);
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+
+        }
+    }
+}
