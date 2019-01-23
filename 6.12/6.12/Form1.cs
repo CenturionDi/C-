3e4998eb3cfb89d120e0f4e9dca23cd08aaa1152
CenturionDi <40587389+CenturionDi@users.noreply.github.com>
2019-01-23 22:10:22 +0600

    6.12.2018
    6.12.2018

---
+++ 6.12/6.12/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Database;Integrated Security=SSPI;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString)) {
+                connection.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("Select * from clients", connection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                this.dataGridView1.DataSource = dataTable;
+            }
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("Select * from traders", connection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                this.dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("Select * from sales", connection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                this.dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
