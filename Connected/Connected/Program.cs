91488db6f61ae15d2b3f776366542444e5679ea9
CenturionDi <40587389+CenturionDi@users.noreply.github.com>
2019-01-23 20:50:43 +0600

    1.12.2018
    1.12.2018

---
+++ Connected/Connected/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionStrings"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            
+            connection.Open();
+
+            var command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "CREATE TABLE gruppa (id int primary key identity(1,1), name nvarchar(max)); ";
+            command.ExecuteNonQuery();
+        }
+    }
+}
