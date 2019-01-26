1fb500e0c0d11c75caeb4ca85bdbec2b69907550
CenturionDi <40587389+CenturionDi@users.noreply.github.com>
2019-01-26 16:32:51 +0600

    22.12
    22.12

---
+++ Onlineshop/Onlineshop/ContextShop.cs
@@ -0,0 +1,30 @@
+﻿namespace Onlineshop
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ContextShop : DbContext
+    {
+        // Контекст настроен для использования строки подключения "ContextShop" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "Onlineshop.ContextShop" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "ContextShop" 
+        // в файле конфигурации приложения.
+        public ContextShop()
+            : base("name=ContextShop")
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Good> Goods { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ Onlineshop/Onlineshop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onlineshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ContextShop())
+            {
+                var customers = context.Customers.ToList();
+                context.Customers.Add(new Customer
+                {
+                    Id = 1,
+                    Name = "Ron",
+                    Surname = "Black",
+                    Phone = "32423534"
+
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
